fd7e85405f4b3ddc99a633f61f33f562f88cc437
Adhi Ardiansyah <adhiardiansyah23@gmail.com>
2021-09-23 14:25:36 +0700

    user update

--- dto/user-dto.go
+++ dto/user-dto.go
@@ -5,4 +5,6 @@ type UserUpdateDTO struct {
 	Nama     string `json:"nama" form:"nama" binding:"required"`
 	Email    string `json:"email" form:"email" binding:"required,email"`
 	Password string `json:"password,omitempty" form:"password,omitempty"`
+	Telp     string `json:"telp" form:"telp" binding:"required"`
+	Alamat   string `json:"alamat" form:"alamat" binding:"required"`
 }
