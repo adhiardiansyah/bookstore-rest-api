d414402189c054cac52610ab51927f5f3b487789
Adhi Ardiansyah <adhiardiansyah23@gmail.com>
2021-11-11 16:13:19 +0700

    edit register DTO

--- dto/register-dto.go
+++ dto/register-dto.go
@@ -1,8 +1,8 @@
 package dto
 
 type RegisterDTO struct {
-	Nama     string `json:"nama" form:"nama" binding:"required"`
-	Email    string `json:"email" form:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
-	RoleID   int    `json:"role_id,omitempty" form:"role_id,omitempty"`
+	Nama     string  `json:"nama" form:"nama" binding:"required"`
+	Email    string  `json:"email" form:"email" binding:"required,email"`
+	Password *string `json:"password" binding:"required"`
+	RoleID   int     `json:"role_id,omitempty" form:"role_id,omitempty"`
 }
